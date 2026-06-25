dd9f22c3b818b4c3b440fd079fb8616bfc0c99ec
Анастасия Логвин <anastasialogvin@MacBook-Air-Anastasia-4.local>
2026-06-25 16:32:31 +0300

    fixed E128 errors in test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -40,12 +40,18 @@ def test_should_convert_age_correctly(
 @pytest.mark.parametrize(
     "cat_age, dog_age, expected_exception",
     [
-        pytest.param(-1, -1, ValueError,
-            id="test should raise exception when age is negative"),
-        pytest.param(None, "", TypeError,
-            id="test should raise exception when no age is given"),
-        pytest.param(3.5, "1", TypeError,
-            id="test should raise exception when input is not an integer")
+        pytest.param(
+            -1, -1, ValueError,
+            id="test should raise exception when age is negative"
+        ),
+        pytest.param(
+            None, "", TypeError,
+            id="test should raise exception when no age is given"
+        ),
+        pytest.param(
+            3.5, "1", TypeError,
+            id="test should raise exception when input is not an integer"
+        )
     ]
 )
 def test_should_raise_exception_if_arguments_are_invalid(
